6226b7f229dc9c0b31030a25a9432dd66f186a44
iisaev <iisaev@solanteq.com>
2016-02-28 00:26:40 +0300

    directory visiting, files parsing

--- app/indexer/Main.hs
+++ app/indexer/Main.hs
@@ -1,13 +1,45 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE QuasiQuotes          #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
 
 module Main where
 
 import qualified Data.Map        as M
 import           Prelude         hiding (readFile, writeFile)
-import           Text.Hamlet.XML
-import           Text.XML
-import Lib
+import           System.Environment
+import           System.IO
+import           System.Directory
+import           Data.Array.IO
+import           Lib
+import           Data.List
+
+isXmlFile :: FilePath -> Bool
+isXmlFile f = isSuffixOf ".xml" f
+
+visitFileTree :: String -> IO ([String])
+visitFileTree path = do
+  isDir <- doesDirectoryExist path
+  if isDir
+     then do
+       dirContents <- getDirectoryContents path
+       files <- mapM visitFileTree $ (\n -> path ++ ('/':n)) <$> filter isXmlFile dirContents
+       return $ concat files
+     else return $ path:[]
+
+getFilesArray :: [String] -> IO(IOArray Int String)
+getFilesArray files = newListArray (1, length files) files
 
 main :: IO ()
-main = someFunc
+main = do
+  args <- getArgs
+  allFiles <- mapM visitFileTree args
+  let files = concat allFiles
+  xmls <- mapM (\f -> (openFile f ReadMode) >>= hGetContents) files
+  let corpuses = parseCorpus <$> xmls
+  fileArr <- getFilesArray files
+  assocs <- getAssocs fileArr
+  print assocs
+  print corpuses
+
+  return ()
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,40 @@
 module Lib
-    ( someFunc
+    (
+      Corpus(..),
+      Index(..),
+      parseCorpus
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+
+import Text.XML.Light
+import Data.List
+import qualified Data.Map.Lazy as M
+import Data.Array
+
+data Corpus = Corpus { title :: String
+                     , snippets :: [String]
+                     } deriving (Show)
+
+type Path = String
+type Term = String
+type Occurence = (Int, Int) -- first - number of document, second - number of snippet, 0 - title
+
+data Index = Index { paths :: Array Int (Path, Corpus)
+                   , termins :: M.Map Term (Array Int Occurence)
+                     } deriving (Show)
+
+
+getC :: String -> Element -> [Element]
+getC str = findChildren (unqual str)
+
+getCs :: String -> [Element] -> [Element]
+getCs str docs = concat $ findChildren (unqual str) <$> docs
+
+parseCorpus :: String -> Corpus -- maybe instance Read ?
+parseCorpus str =
+  let doc = (onlyElems $ parseXML str) !! 0
+      corpTitle = intercalate " " $ strContent <$>
+        (getCs "title" $ getCs "titleStmt" $ getCs "fileDesc" $ getC "teiHeader" doc)
+      body = getCs "body" $ getC "text" doc
+      corpBody = strContent <$> (getCs "head" body) ++ (getCs "p" body)
+   in Corpus { title = corpTitle, snippets = corpBody}
